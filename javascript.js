190d059202915e347aeaa0443dd0d4eef978af37
drunkentaco <134489107+Drunkentaco@users.noreply.github.com>
2024-11-12 19:13:23 -0500

    Redue some codes without using loops to challenge myself!
    Added messages in HTML for players

--- javascript.js
+++ javascript.js
@@ -18,10 +18,12 @@ function getHumanChoice(){
     return choice = playerInput.toLowerCase();
 }
 
-function playGame(){
+let roundNumber = 0
 
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame(){
    
     function playRound(humanChoice, computerChoice){
         if (humanChoice === computerChoice){
@@ -32,41 +34,55 @@ function playGame(){
             (humanChoice === "scissor" && computerChoice === "rock")
         ){    
             return "You lose!";
-        } else if (
+        } else if(
             (humanChoice === "rock" && computerChoice === "scissor")||
             (humanChoice === "paper" && computerChoice === "rock") ||
             (humanChoice === "scissor" && computerChoice === "paper")
         ){
-            return "You win!"
-        }else return getHumanChoice();
+            return "You win!";
+        }else( humanChoice === undefined)
+            return playGame()
     }
 
-    for (let i = 0; i < 5; i++){
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        console.log("Round " + (i + 1) + ":");
-        console.log("You choose " + humanSelection);
-        console.log("I choose " + computerSelection);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
     
-        const roundResult = playRound(humanSelection, computerSelection);
+    console.log("Round " + (roundNumber + 1) + ":");
+    console.log("You choose " + humanSelection);
+    console.log("I choose " + computerSelection);
+    
+    const roundResult = playRound(humanSelection, computerSelection);
         if(roundResult === "You win!"){
-            humanScore++, console.log("You win... " + humanSelection + " beats " + computerSelection + " (˃̣̣̥ᯅ˂̣̣̥)");
+            humanScore++, roundNumber++, 
+            console.log("You win... " + humanSelection + " beats " + computerSelection + " (˃̣̣̥ᯅ˂̣̣̥)"),
+            finalResult();
         }else if (roundResult === "You lose!"){
-            computerScore++, console.log("You lose! " + computerSelection + " beats " + humanSelection + " (˵ •̀ᴗ•́˵)و")
+            computerScore++, roundNumber++
+            console.log("You lose! " + computerSelection + " beats " + humanSelection + " (˵ •̀ᴗ•́˵)و"),
+            finalResult();
         }else if (roundResult === "Draw"){
-            console.log("It's a draw ಠ_ಠ")
+            roundNumber++,
+            console.log("It's a draw ಠ_ಠ"),
+            finalResult();
         }else return
-    }
-
-    console.log("Game over!")
+        
+    function finalResult(){
+        if (roundNumber < 5){
+            return playGame();
+        }else(roundNumber = 5)
+            return gameOver();
+        }   
+    
+    function gameOver(){
+        console.log("Game over!")
         if (humanScore > computerScore){
-            return console.log(humanScore + " || " + computerScore + " Darn it, you won the game (っ- ‸ - ς)")
+            console.log(humanScore + " || " + computerScore + " Darn it, you won the game (っ- ‸ - ς)")
         }else if (computerScore > humanScore){
-            return console.log(humanScore + " || " + computerScore + " YYYYYEEEEEEEESSSSSSS I WINNNNN <(^-^<) <( ^-^ )> (>^-^)>")
+            console.log(humanScore + " || " + computerScore + " YYYYYEEEEEEEESSSSSSS I WINNNNN <(^-^<) <( ^-^ )> (>^-^)>")
         }else{
             console.log(humanScore + " || " + computerScore + " It's a tie ... how boring (¬_¬)")
         }
+    }
+    
 }
-playGame();
+playGame()
