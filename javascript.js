369093827bed1fbb825f926cca7bb0969fbc4262
drunkentaco <134489107+Drunkentaco@users.noreply.github.com>
2024-12-02 18:27:35 -0500

    Worked out all the bugs! Althought the winner was made, it was still going through rounds!

--- javascript.js
+++ javascript.js
@@ -2,23 +2,22 @@ let roundNumber = 0;
 let humanScore = 0;
 let computerScore = 0;
 
+function getComputerChoice() {
+    let randomNumber = Math.random()
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber >= 0.33 && randomNumber < 0.66) {
+        return "paper";
+    } else (randomNumber >= 0.66)
+        return "scissor";
+}
+
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {    
 
     button.addEventListener('click', (event) => {
-        let humanSelection = event.target.id;
-        
-        function getComputerChoice() {
-            let randomNumber = Math.random()
-            if (randomNumber < 0.33) {
-               return "rock";
-            } else if (randomNumber >= 0.33 && randomNumber < 0.66) {
-               return "paper";
-            } else (randomNumber >= 0.66)
-                return "scissor";
-        }
-        
+        const humanSelection = event.target.id;
         const computerSelection = getComputerChoice();
         
         function playRound(humanChoice, computerChoice) {
@@ -36,48 +35,48 @@ buttons.forEach((button) => {
                 (humanChoice === "scissor" && computerChoice === "paper")
             )
                 return "You win!";
+            
         }
 
         const selection = document.querySelector('#selection');
-        selection.textContent = `You choose ${humanSelection}, I choose ${computerSelection}.`   
-    
         const result = document.querySelector('#result')
 
-        const roundResult = playRound(humanSelection, computerSelection);
-        if(roundResult === "You win!"){
-            ++humanScore, ++roundNumber,
-            result.textContent = `You win... ${humanSelection} beats ${computerSelection} (˃̣̣̥ᯅ˂̣̣̥)`,
-            finalResult();
-        }else if (roundResult === "You lose!"){
-            ++computerScore, ++roundNumber,
-            result.textContent = `You lose! ${computerSelection} beats ${humanSelection} (˵ •̀ᴗ•́˵)و`,
-            finalResult();
-        }else if (roundResult === "Draw"){
-            ++roundNumber,
-            result.textContent = "It's a draw ಠ_ಠ",
-            finalResult();
-        }else return
+        if (humanScore !== 5 && computerScore !==5){
+            selection.textContent = `You choose ${humanSelection}, I choose ${computerSelection}.`  
+            const roundResult = playRound(humanSelection, computerSelection);
+            if(roundResult === "You win!"){
+                ++humanScore, ++roundNumber,
+                result.textContent = `You win... ${humanSelection} beats ${computerSelection} (˃̣̣̥ᯅ˂̣̣̥)`;
+            }else if (roundResult === "You lose!"){
+                ++computerScore, ++roundNumber,
+                result.textContent = `You lose! ${computerSelection} beats ${humanSelection} (˵ •̀ᴗ•́˵)و`;
+            }else if (roundResult === "Draw"){
+                ++roundNumber,
+                result.textContent = "It's a draw ಠ_ಠ";
+            }else return
+        }
       
         const score = document.querySelector('#score');
         score.textContent = `Ugly Human: ${humanScore} || Sexy Computer: ${computerScore}`
 
         const round = document.querySelector('#round');
         round.textContent = `Round ${roundNumber}:`;
-    
-        function finalResult(){
-            if (humanScore === 5 || computerScore === 5){
-                const over = document.querySelector('#over');
-                const final = document.querySelector('#final');
-                over.textContent = 'GAME OVER!!!'
         
-                if (humanScore === 5){
-                    final.textContent = `Darn it, you won the game (っ- ‸ - ς)`,
-                    event.stopPropagation()
-                }else (computerScore === 5)
-                    final.textContent = `YYYYYEEEEEEEESSSSSSS I WINNNNN <(^-^<) <( ^-^ )> (>^-^)>`,
-                    event.stopPropagation()
-            }else
-                return;
-        }   
+
+    
+        const over = document.querySelector('#over');
+        const final = document.querySelector('#final');
+        const restart = document.querySelector('#restart');
+
+        if (humanScore === 5){
+            over.textContent = "GAME OVER!!!",
+            final.textContent = `Darn it, you won the game (っ- ‸ - ς)`;
+            restart.textContent= 'Please reload the game to play again!'
+                }
+        if (computerScore === 5){
+            over.textContent = "GAME OVER!!!",
+            final.textContent = `YYYYYEEEEEEEESSSSSSS I WINNNNN <(^-^<) <( ^-^ )> (>^-^)>`;
+            restart.textContent = 'Please reload the game to play again!'
+            } 
     });
 });
